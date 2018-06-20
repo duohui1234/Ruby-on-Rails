f1f741fa8a5c5471350174e6674a54ced15261fb
정다혜 <duohui12@gmail.com>
2018-06-20 21:49:50 +0900

    put,delete method

--- asked/config/routes.rb
+++ asked/config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
-  get 'posts/index'   => 'posts#index'
-  get 'posts/new'    => 'posts#new'
-  get 'posts/create'
-  
-  get 'posts/:id/show'    => 'posts#show'
+  get 'posts/index'
+  get 'posts/new'
+  post 'posts/create'
+
+  get 'posts/:id'    => 'posts#show'
   get 'posts/:id/edit'  => 'posts#edit'
-  get 'posts/:id/update'   => 'posts#update'
-  get 'posts/:id/destroy'   => 'posts#destroy'
+  put 'posts/:id'   => 'posts#update'
+  delete 'posts/:id'   => 'posts#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
